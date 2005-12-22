ef68ff46219d99a05a9450ad4cb777fb663eafc6
Igor Sukhih <igor@openvz.org>
2005-12-22 13:04:58 +0000

    Fixed 2level quota links creation

--- src/lib/script.c
+++ src/lib/script.c
@@ -253,13 +253,13 @@ int mk_quota_link()
 		logger(0, errno, "Unable to create %s", buf);
 	snprintf(buf, sizeof(buf), PROC_QUOTA "%08lx" QUOTA_U,
 		(unsigned long)st.st_dev);
-	if (lstat(QUOTA_U, &st))
-		if (symlink(buf, QUOTA_U)) 
-			logger(0, errno, "Unable to create symlink %s", buf); 
+	unlink(QUOTA_U);
+	if (symlink(buf, QUOTA_U)) 
+		logger(0, errno, "Unable to create symlink %s", buf); 
 	snprintf(buf, sizeof(buf), PROC_QUOTA "%08lx" QUOTA_G,
 		(unsigned long)st.st_dev);
-	if (lstat(QUOTA_G, &st))
-		if (symlink(buf, QUOTA_G))
-			logger(0, errno, "Unable to create symlink %s", buf); 
+	unlink(QUOTA_G);	
+	if (symlink(buf, QUOTA_G))
+		logger(0, errno, "Unable to create symlink %s", buf); 
 	return 0;
 }
