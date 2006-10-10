50795b6e568747ef8398a536bcc100caf33369bb
Thorsten Schifferdecker <tsd@debian.systs.org>
2006-10-10 14:20:05 +0400

    Changed environment on VE enter.
    When enter a VE the $HOME variable is set to /, but if I start a program, which
    set it's personal config files to $HOME, the ROOTDIR of VE is filled with
    config-settings.
    
    http://bugzilla.openvz.org/show_bug.cgi?id=296

--- src/enter.c
+++ src/enter.c
@@ -256,8 +256,8 @@ int do_enter(vps_handler *h, envid_t veid, char *root)
 			char buf[64];
 			char *term;
 		        char *arg[] = {"-bash", NULL};
-		        char *env[] = {"HOME=/", "HISTFILE=/dev/null",
-				"PATH=/bin:/sbin:/usr/bin:/usr/sbin:", NULL, NULL};
+			char *env[] = {"PATH=/bin:/sbin:/usr/bin:/usr/sbin:", "HISTFILE=/dev/null",
+				"USER=root", "HOME=/root", "LOGNAME=root", NULL, NULL};
 			close(master);
 			set_ctty(slave);
 			dup2(slave, 0);
