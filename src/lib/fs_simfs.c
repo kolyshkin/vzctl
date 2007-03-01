e07fecea0b78db77e2873b3588aec73ca477301c
Kir Kolyshkin <kir@openvz.org>
2007-03-01 17:32:57 +0300

    vzctl mount: print warning if no simfs
    In case simfs mount failed returning ENODEV, print a hint.
    http://bugzilla.openvz.org/429

--- src/lib/fs_simfs.c
+++ src/lib/fs_simfs.c
@@ -83,6 +83,9 @@ int vz_mount(fs_param *fs, int remount)
 			remount ? "" : fs->private) < 0)
 	{
 		logger(-1, errno, "Can't mount: %s %s", fs->root, fs->private);
+		if (errno == ENODEV)
+			logger(-1, errno, "Kernel lacks simfs support. Please "
+				"compile it in, or load simfs module.");
 		return VZ_FS_CANTMOUNT;
 	}
 	return 0;
