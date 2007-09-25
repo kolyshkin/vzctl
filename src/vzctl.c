277c60cc6b1137bdb4e98b7a2bbe3846e51ea6f7
Igor Sukhih <igor@openvz.org>
2007-09-25 16:14:37 +0400

    Added '\n' to the error message

--- src/vzctl.c
+++ src/vzctl.c
@@ -268,7 +268,7 @@ int main(int argc, char *argv[], char *envp[])
 		goto error;
 	}
 	if (veid == 0 && action != ACTION_SET) {
-		fprintf(stderr, "Only set actions allowed for VE0");
+		fprintf(stderr, "Only set actions are allowed for VE0\n");
 		ret = VZ_INVALID_PARAMETER_VALUE;
 		goto error;
 	} else if (veid < 0) {
