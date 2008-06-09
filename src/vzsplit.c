c5bf4bb6743f27e477dab9f3c8b2d5dad5306b4c
Thorsten Schifferdecker <curx@openvz.org>
2008-06-09 15:22:59 +0400

    vzsplit: fix a typo

--- src/vzsplit.c
+++ src/vzsplit.c
@@ -176,7 +176,7 @@ char *level_string[MAX_SL+1] = {
 void usage(int rc)
 {
 	fprintf(rc ? stderr : stdout, "Usage: vzsplit [-f config_name] "
-			"| [-n numves] | [-s swap_size\n"
+			"| [-n numves] | [-s swap_size]\n"
 		"\t -f specified config name\n"
 		"\t -n specified number of containers\n"
 		"\t -s specified swap in Kbytes\n");
