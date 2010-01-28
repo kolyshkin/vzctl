kolyshkin/vzctl

commit 4ed54c389ab25e7dd8d61405cf60b53e8a38aca2

debian-add_ip.sh: minor cleanup in get_all_aliasid()
* Break the long line
* Use $(...) instead of `...` for better readability
* Use tab instead of 8 spaces

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

etc/dists/scripts/debian-add_ip.sh
@@ -105,7 +105,8 @@ iface ${VENET_DEV}:${ifnum} inet static
 function get_all_aliasid()
 {
 	IFNUM=-1
-        IFNUMLIST=`grep -e "^auto ${VENET_DEV}:.*$" 2> /dev/null ${CFGFILE}.bak | sed "s/.*${VENET_DEV}://"`
+	IFNUMLIST=$(grep -e "^auto ${VENET_DEV}:.*$" 2>/dev/null
+		${CFGFILE}.bak | sed "s/.*${VENET_DEV}://")
 }
 
 function get_free_aliasid()

commit a24947f1ddb4ff1d9029b16c1f7d33f326431dc4

debian-add_ip.sh: small fixup
A recent commit 4ed54c389ab25e7dd8d61405cf60b53e8a38aca2
introduced a bug in shell code (missing backslash). Fix it.

http://bugzilla.openvz.org/show_bug.cgi?id=1418

etc/dists/scripts/debian-add_ip.sh
@@ -108,7 +108,7 @@ iface ${VENET_DEV}:${ifnum} inet static
 function get_all_aliasid()
 {
 	IFNUM=-1
-	IFNUMLIST=$(grep -e "^auto ${VENET_DEV}:.*$" 2>/dev/null
+	IFNUMLIST=$(grep -e "^auto ${VENET_DEV}:.*$" 2>/dev/null \
 		${CFGFILE}.bak | sed "s/.*${VENET_DEV}://")
 }
 