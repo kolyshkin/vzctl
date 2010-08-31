8785cbd242692f02f5e60e4d7a30000b2a006cb9
Kir Kolyshkin <kir@openvz.org>
2010-08-31 13:08:57 +0400

    Do not allow INT_MAX CT ID
    vzctl (and mee, too) thought that INT_MAX makes perfectly valid
    CT ID. Unfortunately kernel doesn't agree with us, failing to start
    such a container with the following diagnostics:
    
    	Can't create fairsched node 2147483647
    	CT: 2147483647: stopped
    	CT: 2147483647: failed to start with err=-16
    
    This is because in fair scheduler node with ID of MAX_INT
    is reserved for CT0. So check for that CTID and complain that
    it's invalid when trying to create it, rather than let it be
    created and then failed to start.
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- src/vzctl.c
+++ src/vzctl.c
@@ -228,7 +228,7 @@ int main(int argc, char *argv[], char *envp[])
 	if (parse_int(argv[2], &veid)) {
 		name = strdup(argv[2]);
 		veid = get_veid_by_name(name);
-		if (veid < 0) {
+		if (veid < 0 || veid == INT_MAX) {
 			fprintf(stderr, "Bad CT ID %s\n", argv[2]);
 			ret = VZ_INVALID_PARAMETER_VALUE;
 			goto error;
