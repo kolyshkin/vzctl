2b1f193eaa6d18b13916165f2e9c3900c7d1e6cb
Kir Kolyshkin <kir@openvz.org>
2010-12-23 19:41:19 +0300

    vzctl set --dev*: return proper error, document it
    This patch fixes the error code returned by vzctl in case
    it failed to configure devices inside a CT.
    
    While at it, document this error code in vzctl(8) man page
    and make an error message in set_devperm() more human-readable.
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- src/lib/dev.c
+++ src/lib/dev.c
@@ -93,7 +93,6 @@ static int dev_create(const char *root, dev_res *dev)
 
 int set_devperm(vps_handler *h, envid_t veid, dev_res *dev)
 {
-	int ret;
 	struct vzctl_setdevperms devperms;
 
 	devperms.veid = veid;
@@ -101,10 +100,12 @@ int set_devperm(vps_handler *h, envid_t veid, dev_res *dev)
 	devperms.mask = dev->mask;
 	devperms.type = dev->type;
 
-	if ((ret = ioctl(h->vzfd, VZCTL_SETDEVPERMS, &devperms)))
-		logger(-1, errno, "Unable to set devperms");
+	if (ioctl(h->vzfd, VZCTL_SETDEVPERMS, &devperms)) {
+		logger(-1, errno, "Error setting device permissions");
+		return VZ_SET_DEVICES;
+	}
 
-	return ret;
+	return 0;
 }
 
 struct vzctl_ve_configure_pci {
@@ -164,10 +165,11 @@ int vps_set_devperm(vps_handler *h, envid_t veid, const char *root,
 	list_for_each(res, dev_h, list) {
 		if (res->name)
 			if ((ret = dev_create(root, res)))
-				break;
+				goto out;
 		if ((ret = set_devperm(h, veid, res)))
-			break;
+			goto out;
 	}
+out:
 	return ret;
 }
 
