kolyshkin/vzctl

commit 9612a012177d34a7c3407ccf168dfbadbd2c89f4

debian-{add,del}_ip.sh: fix IPv6 wrt 2.6.32 kernel
When vzctl attempts to add an IPv6 address to a Debian Lenny container on
2.6.32 no error is returned but the IP address is not added to the container.

vz-1:~# vzctl set 228 --ipadd 2001:0DB8::1
Adding IP address(es): 2001:db8::1
WARNING: Settings were not saved and will be resetted to original values on
next start (use --save flag)
vz-1:~# vzctl enter 228
entered into VE 228
228:/# ifconfig
lo        Link encap:Local Loopback
          inet addr:127.0.0.1  Mask:255.0.0.0
          inet6 addr: ::1/128 Scope:Host
          UP LOOPBACK RUNNING  MTU:16436  Metric:1
          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
          collisions:0 txqueuelen:0
          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)

venet0    Link encap:UNSPEC  HWaddr
00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00
          inet addr:127.0.0.1  P-t-P:127.0.0.1  Bcast:0.0.0.0
Mask:255.255.255.255
          UP BROADCAST POINTOPOINT RUNNING NOARP  MTU:1500  Metric:1
          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
          collisions:0 txqueuelen:0
          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)

This appears to be due to the way the interface is specified in
/etc/network/interfaces. The current script generates IPv6 configurations in
the following form:

iface venet0 inet6 static
    address ::1
    netmask 128
    up ifconfig venet0 add 2001:db8::1/0

However this does not work with the 2.6.32 kernel:

228:/# ifup -v venet0
Configuring interface venet0=venet0 (inet)
run-parts --verbose /etc/network/if-pre-up.d

ifconfig venet0 127.0.0.1 netmask 255.255.255.255 broadcast 0.0.0.0
up

route add default dev venet0
run-parts --verbose /etc/network/if-up.d
run-parts: executing /etc/network/if-up.d/mountnfs
run-parts: executing /etc/network/if-up.d/openssh-server
Configuring interface venet0=venet0 (inet6)

etc/dists/scripts/debian-add_ip.sh
@@ -76,18 +76,16 @@ iface ${LOOPBACK} inet loopback" >> ${CFGFILE}
 		echo "
 # Auto generated ${VENET_DEV} interface
 auto ${VENET_DEV}
-iface ${VENET_DEV} inet static
-	address 127.0.0.1
-	netmask 255.255.255.255
-	broadcast 0.0.0.0
+iface ${VENET_DEV} inet manual
+	up ifconfig ${VENET_DEV} up
 	up route add default dev ${VENET_DEV}
+	down route del default dev ${VENET_DEV}
+	down ifconfig ${VENET_DEV} down
 " >> ${CFGFILE}
 
 		if [ "${IPV6}" = "yes" ]; then
 			echo "
-iface venet0 inet6 static
-	address ::1
-	netmask 128
+iface venet0 inet6 manual
 " >> ${CFGFILE}
 
 		fi
@@ -110,11 +108,10 @@ function create_config()
 iface ${VENET_DEV}:${ifnum} inet static
 	address ${ip}
 	netmask 255.255.255.255
-	broadcast 0.0.0.0
 " >> ${CFGFILE}.bak
 
 	else
-	    sed -i -e "s/netmask\ 128/netmask\ 128\n\tup ifconfig venet0 add ${ip}\/0/" ${CFGFILE}.bak
+	    sed -i -e "s/iface ${VENET_DEV} inet6 manual/iface ${VENET_DEV} inet6 manual\n\tup ifconfig ${VENET_DEV} add ${ip}\/0\n\tdown ifconfig ${VENET_DEV} del ${ip}\/0/" ${CFGFILE}.bak
 	fi
 
 }

etc/dists/scripts/debian-del_ip.sh
@@ -39,8 +39,8 @@ function del_ip()
 
 		else
 		    grep -v ${ip} /etc/network/interfaces > ${CFGFILE}.bak
+		    ifconfig ${VENET_DEV} del ${ip}/0
 		    mv ${CFGFILE}.bak ${CFGFILE}
-		    /etc/init.d/networking restart > /dev/null 2>&1
 		fi
 	done
 }

commit 83d23dbf5470f63a7e2ff4e0c6b30995b1dbabc2

etc/dists/scripts/debian-add_ip.sh: fix not adding default route
This fixes the problem of non-working network in Debian and Ubuntu
containers after upgrade to vzctl-3.0.26, caused by commit 9612a0121.

Apparently, older kernels (like 2.6.18) are refusing to add default
route for a device with no IP addresses. Such problem does not exist
with RHEL6 based (2.6.32) kernel, but RHEL5-based kernels (2.6.18)
are failing miserably.

The workaround is to use 'ifconfig venet0 0' which apparently adds
something like 0.0.0.0/0 IP to venet0, after which "route add" works.

The issue is reported in bug #1792:
http://bugzilla.openvz.org/1792

It is also referred to in bug #1669 (comment #6):
http://bugzilla.openvz.org/1669

[kir@openvz.org: changelog entry]

etc/dists/scripts/debian-add_ip.sh
@@ -78,6 +78,7 @@ iface ${LOOPBACK} inet loopback" >> ${CFGFILE}
 auto ${VENET_DEV}
 iface ${VENET_DEV} inet manual
 	up ifconfig ${VENET_DEV} up
+	up ifconfig ${VENET_DEV} 0
 	up route add default dev ${VENET_DEV}
 	down route del default dev ${VENET_DEV}
 	down ifconfig ${VENET_DEV} down