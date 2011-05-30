d99f65879a6d708aa80f47690e114f1c15004fae
Kir Kolyshkin <kir@openvz.org>
2011-05-31 00:44:42 +0400

    man/html2wiki.sh: comment the code
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- man/html2wiki.sh
+++ man/html2wiki.sh
@@ -1,8 +1,27 @@
+#!/bin/bash
+#
+# Post-process filter for groff -Thtml for our wiki
+
+# Prepare sed expression to replace known man pages refs with wiki links
 for M in $MANS vzquota.8; do
 	MM=$(echo $M | sed -e 's/^/<b>/' -e 's/.\([58]\)/<\/b>(\1)/')
 	REPL="$REPL -e s#$MM#[[Man/$M|$MM]]#"
 done
 
+# The below code tries to:
+#  (first sed invocation)
+#  - remove everything except contents of <body>...</body>
+#  - remove <h1>, <hr> and section links
+#  - remove section anchors and closing </hN> tags
+#  - change <h2>TEXT to == TEXT ==
+#  - use wiki-style links for wiki.openvz.org
+#  - apply the above expression for man pages refs
+#  (awk)
+#  - combine two lines with broken third-level headings (=== ... ===)
+#  (second sed invocation)
+#  - remove the crap before/after third-level headings
+#  (cat -s)
+#  - suppress repeated empty lines
 sed -e '1,/<body>/d' -e '/<\/body>/,$d' \
     -e '/<h1/d' -e '/<a href=/d' -e '/<hr>/d' \
     -e '/<a name=/d' -e '/<\/h[123456]>/d' \
