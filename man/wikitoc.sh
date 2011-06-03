45e7cfbfd8f00c2fb864f4f7c010566765948e44
Kir Kolyshkin <kir@openvz.org>
2011-06-04 02:56:47 +0400

    man/Makefile.am: add wiki.toc target
    This is to create a table-of-contents like page for OpenVZ wiki,
    to be used on http://wiki.openvz.org/Man
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

---
+++ man/wikitoc.sh
@@ -0,0 +1,8 @@
+for FILE in $*; do
+	NAME=$(echo $FILE | sed 's/\.\([0-9]\)$/ (\1)/')
+	HDR=$(grep -A1 '^\.SH NAME$' $FILE | tail -1 | \
+		sed 's/^.*\\- //' | sed 's/\.$//')
+	echo "|-"
+	echo "|| [[/$FILE/|$NAME]]"
+	echo "|| $HDR"
+done
