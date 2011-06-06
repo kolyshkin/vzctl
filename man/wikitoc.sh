kolyshkin/vzctl

commit d0e61740c13ca06ebe51556aa615d931332e64d6

man/wikitoc.sh: use Template:Man
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

man/wikitoc.sh
@@ -1,8 +1,9 @@
 for FILE in $*; do
-	NAME=$(echo $FILE | sed 's/\.\([0-9]\)$/\&nbsp;(\1)/')
+	NAME=$(echo $FILE | awk -F . '{print $1}')
+	SECT=$(echo $FILE | awk -F . '{print $2}')
 	HDR=$(grep -A1 '^\.SH NAME$' $FILE | tail -1 | \
 		sed 's/^.*\\- //' | sed 's/\.$//')
 	echo "|-"
-	echo "|| [[/$FILE/|$NAME]]"
+	echo "|| {{Man|$NAME|$SECT}}"
 	echo "|| $HDR"
 done

commit 9b80a197de0c0980f66f92c4655831a0fcebf464

man/wikitoc.sh: fix getting section number from file name
It was broken in commit d0e61740

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

man/wikitoc.sh
@@ -1,6 +1,6 @@
 for FILE in $*; do
-	NAME=$(echo $FILE | awk -F . '{print $1}')
-	SECT=$(echo $FILE | awk -F . '{print $2}')
+	NAME=$(echo $FILE | sed 's/\.[0-9]$//')
+	SECT=$(echo $FILE | sed 's/^.*\.\([0-9]\)$/\1/')
 	HDR=$(grep -A1 '^\.SH NAME$' $FILE | tail -1 | \
 		sed 's/^.*\\- //' | sed 's/\.$//')
 	echo "|-"