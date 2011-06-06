a9bfef355990a8d1ecddda642ee0e5e8afe9cf5f
Kir Kolyshkin <kir@openvz.org>
2011-06-06 16:05:10 +0400

    man/wikitoc.sh: use &nbsp; between a man page and a section number
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- man/wikitoc.sh
+++ man/wikitoc.sh
@@ -1,5 +1,5 @@
 for FILE in $*; do
-	NAME=$(echo $FILE | sed 's/\.\([0-9]\)$/ (\1)/')
+	NAME=$(echo $FILE | sed 's/\.\([0-9]\)$/\&nbsp;(\1)/')
 	HDR=$(grep -A1 '^\.SH NAME$' $FILE | tail -1 | \
 		sed 's/^.*\\- //' | sed 's/\.$//')
 	echo "|-"
