9a8350a4b9102383a8c457ecee409afc6d73c6e6
Kir Kolyshkin <kir@openvz.org>
2011-08-21 22:22:56 +0400

    setver.sh: do not hardcore configure.ac and vzctl.spec
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- setver.sh
+++ setver.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+CONFIGURE_AC=configure.ac
+RPM_SPEC=vzctl.spec
+
 # Try to figure out version from git
 GIT_DESC=$(git describe --tags | sed s'/^[^0-9]*-\([0-9].*\)$/\1/')
 GIT_V=$(echo $GIT_DESC | sed 's/-.*$//')
@@ -8,43 +11,43 @@ GIT_R=$(echo $GIT_DESC | sed 's/^[^-]*-\([1-9][0-9]*\)-g/\1.git./')
 test "$GIT_V" = "$GIT_R" && GIT_R="1"
 GIT_VR="${GIT_V}-${GIT_R}"
 
-CONF_V=$(grep AC_INIT configure.ac | \
+CONF_V=$(grep AC_INIT $CONFIGURE_AC | \
 		sed 's/^[^,]*,[ ]\([^,]*\),.*$/\1/')
 
 read_spec() {
-	SPEC_V=$(awk '($1 == "Version:") {print $2}' vzctl.spec)
-	SPEC_R=$(awk '($1 " " $2 == "%define rel") {print $3}' vzctl.spec)
+	SPEC_V=$(awk '($1 == "Version:") {print $2}' $RPM_SPEC)
+	SPEC_R=$(awk '($1 " " $2 == "%define rel") {print $3}' $RPM_SPEC)
 	SPEC_VR="${SPEC_V}-${SPEC_R}"
 }
 read_spec
 
 # Set version/release in spec from git
 if test "$GIT_VR" != "$SPEC_VR"; then
-#	echo "Changing vzctl.spec:"
+#	echo "Changing $RPM_SPEC:"
 	# Version: 3.0.28
 	# Release: 1%{?dist}
 	sed -i -e "s/^\(Version:[[:space:]]*\).*\$/\1$GIT_V/" \
 	       -e "s/^\(%define rel[[:space:]]*\).*\$/\1$GIT_R/" \
-		vzctl.spec
+		$RPM_SPEC
 	if test "$GIT_R" != "1"; then
 		NVR='%{name}-%{version}-%{rel}'
 		sed -i -e "s/^\(Source:[[:space:]]*\).*\$/\1${NVR}.tar.bz2/" \
 		       -e "s/^%setup[[:space:]]*.*\$/%setup -n ${NVR}/" \
-			vzctl.spec
+			$RPM_SPEC
 	fi
 fi
-#grep -E -H '^Version:|^%define rel|^Source:|^%setup' vzctl.spec
+#grep -E -H '^Version:|^%define rel|^Source:|^%setup' $RPM_SPEC
 
 # Set version in configure.ac from spec
 read_spec
 SPEC_VR=$(echo $SPEC_VR | sed 's/-1$//')
 if test "$CONF_V" != "$SPEC_VR"; then
-#	echo "Changing configure.ac:"
+#	echo "Changing $CONFIGURE_AC:"
 	# AC_INIT(vzctl, 3.0.28, devel@openvz.org)
 	sed -i "s/^\(AC_INIT(vzctl,[ ]\)[^,]*\(,.*\$\)/\1$SPEC_VR\2/" \
-		configure.ac
+		$CONFIGURE_AC
 	autoconf
 fi
-#grep -H '^AC_INIT' configure.ac
+#grep -H '^AC_INIT' $CONFIGURE_AC
 
 exit 0
