kolyshkin/vzctl

commit 57842cb8223d97f6ea20d838b864e4249e53d3f9

etc/dists/scripts/*ip.sh: add IP mask support
Commit f018af5f added support for netmasks. This commit
tries to modify all the *-add_ip.sh and *-del_ip.sh
scripts to understand the netmasks.

There are definitely bugs in this code, as well as some
previous bugs in the modified scripts. I guess it needs
lots of testing and fixing, but let's get this rock rolling!

http://bugzilla.openvz.org/1088

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

etc/dists/scripts/debian-del_ip.sh
@@ -25,12 +25,12 @@ CFGFILE=/etc/network/interfaces
 function del_ip()
 {
 	local ifname
-	local ip
+	local ipm
 
-	for ip in ${IP_ADDR}; do
-		if [ "${ip#*:}" = "${ip}" ]; then
-
-		    ifname=`grep -B 1 -w "${ip}" ${CFGFILE} |
+	for ipm in ${IP_ADDR}; do
+		ip_conv $ipm
+		if [ -z "$_IPV6ADDR ]; then
+		    ifname=`grep -B 1 -w "${_IP}" ${CFGFILE} |
 				grep "${VENET_DEV}:" | cut -d' ' -f2`
 		    if [ -n "${ifname}" ]; then
 				ifdown "${ifname}" 2>/dev/null
@@ -38,8 +38,8 @@ function del_ip()
 		    fi
 
 		else
-		    grep -v ${ip} ${CFGFILE} > ${CFGFILE}.bak
-		    ifconfig ${VENET_DEV} del ${ip}/0
+		    grep -v ${_IP} ${CFGFILE} > ${CFGFILE}.bak
+		    ifconfig ${VENET_DEV} del ${_IP}/${_MASK}
 		    mv ${CFGFILE}.bak ${CFGFILE}
 		fi
 	done

etc/dists/scripts/arch-add_ip.sh
@@ -55,10 +55,11 @@ function setup_network()
 function create_config()
 {
 	local ip=$1
-	local ifnum=$2
+	local netmask=$2
+	local ifnum=$3
 	# add venet0 alias to rc.conf
 	put_param "${CFGFILE}.bak" "${VENET_DEV}_${ifnum}" \
-		"${VENET_DEV}:${ifnum} ${ip} netmask 255.255.255.255 broadcast 0.0.0.0"
+		"${VENET_DEV}:${ifnum} ${ip} netmask ${netmask} broadcast 0.0.0.0"
 
 	# add venet0 alias to INTERFACES array
 	add_param3 "${CFGFILE}.bak" "INTERFACES" "${VENET_DEV}_${ifnum}"
@@ -86,8 +87,7 @@ function get_free_aliasid()
 
 function add_ip()
 {
-	local ip
-	local found
+	local ipm
 	local add
 	local iface
 
@@ -99,13 +99,13 @@ function add_ip()
 
 	setup_network
 
-	for ip in ${IP_ADDR}; do
-		found=
-		if grep -e "\\<${ip}\\>" >/dev/null 2>&1  ${CFGFILE}.bak; then
+	for ipm in ${IP_ADDR}; do
+		ip_conv $ipm
+		if grep -e "\\<${_IP}\\>" >/dev/null 2>&1  ${CFGFILE}.bak; then
 			continue
 		fi
 		get_free_aliasid
-		create_config ${ip} ${IFNUM}
+		create_config "${_IP}" "${_NETMASK}" "${IFNUM}"
 	done
 
 	mv -f ${CFGFILE}.bak ${CFGFILE}

etc/dists/scripts/arch-del_ip.sh
@@ -24,11 +24,12 @@ CFGFILE=/etc/rc.conf
 function del_ip()
 {
 	local ifname
-	local ip
+	local ipm
 
 	echo ${IP_ADDR}
-	for ip in ${IP_ADDR}; do
-		ifname=`grep -B 1 -e "\\<${ip}\\>" ${CFGFILE} |
+	for ipm in ${IP_ADDR}; do
+		ip_conv $ipm
+		ifname=`grep -B 1 -e "\\<${_IP}\\>" ${CFGFILE} |
 			grep "${VENET_DEV}_" | cut -d'=' -f1`
 		if [ -n "${ifname}" ]; then
 		    # shutdown interface venet0_x

etc/dists/scripts/debian-3.x-add_ip.sh
@@ -89,18 +89,20 @@ iface venet0 inet6 static
 function create_config()
 {
 	local ip=$1
-	local ifnum=$2
+	local netmask=$2
+	local mask=$3
+	local ifnum=$4
 
 	if [ "${ip#*:}" = "${ip}" ]; then
 	    echo "auto ${VENET_DEV}:${ifnum}
 iface ${VENET_DEV}:${ifnum} inet static
 	address ${ip}
-	netmask 255.255.255.255
+	netmask ${netmask}
 	broadcast 0.0.0.0
 " >> ${CFGFILE}.bak
 
 	else
-	    sed -i -e "s/netmask\ 128/netmask\ 128\n\tup ifconfig venet0 add ${ip}\/0/" ${CFGFILE}.bak
+	    sed -i -e "s/netmask\ 128/netmask\ 128\n\tup ifconfig venet0 add ${ip}\/${mask}/" ${CFGFILE}.bak
 	fi
 
 }
@@ -126,7 +128,7 @@ function get_free_aliasid()
 
 function add_ip()
 {
-	local ip
+	local ipm
 	local add
 	local iface
 
@@ -150,12 +152,13 @@ function add_ip()
 	fi
 	if [ -n "${IP_ADDR}" ]; then
 		cp -f ${CFGFILE} ${CFGFILE}.bak
-		for ip in ${IP_ADDR}; do
-			if grep -w "${ip}" >/dev/null 2>&1 ${CFGFILE}.bak; then
+		for ipm in ${IP_ADDR}; do
+			ip_conv $ipm
+			if grep -w "${_IP}" >/dev/null 2>&1 ${CFGFILE}.bak; then
 				continue
 			fi
 			get_free_aliasid
-			create_config ${ip} ${IFNUM}
+			create_config "${_IP}" "${_NETMASK}" "${_MASK}" "${IFNUM}"
 		done
 		mv -f ${CFGFILE}.bak ${CFGFILE}
 	fi

etc/dists/scripts/debian-add_ip.sh
@@ -102,17 +102,19 @@ iface ${VENET_DEV} inet6 manual
 function create_config()
 {
 	local ip=$1
-	local ifnum=$2
+	local netmask=$2
+	local mask=$3
+	local ifnum=$4
 
 	if [ "${ip#*:}" = "${ip}" ]; then
 	    echo "auto ${VENET_DEV}:${ifnum}
 iface ${VENET_DEV}:${ifnum} inet static
 	address ${ip}
-	netmask 255.255.255.255
+	netmask ${netmask}
 " >> ${CFGFILE}.bak
 
 	else
-	    sed -i -e "s/iface ${VENET_DEV} inet6 manual/iface ${VENET_DEV} inet6 manual\n\tup ifconfig ${VENET_DEV} add ${ip}\/0\n\tdown ifconfig ${VENET_DEV} del ${ip}\/0/" ${CFGFILE}.bak
+	    sed -i -e "s/iface ${VENET_DEV} inet6 manual/iface ${VENET_DEV} inet6 manual\n\tup ifconfig ${VENET_DEV} add ${ip}\/${mask}\n\tdown ifconfig ${VENET_DEV} del ${ip}\/${mask}/" ${CFGFILE}.bak
 	fi
 
 }
@@ -138,7 +140,7 @@ function get_free_aliasid()
 
 function add_ip()
 {
-	local ip
+	local ipm
 	local add
 	local iface
 
@@ -171,12 +173,13 @@ function add_ip()
 	fi
 	if [ -n "${IP_ADDR}" ]; then
 		cp -f ${CFGFILE} ${CFGFILE}.bak
-		for ip in ${IP_ADDR}; do
-			if grep -w "${ip}" >/dev/null 2>&1 ${CFGFILE}.bak; then
+		for ipm in ${IP_ADDR}; do
+			ip_conv $ipm
+			if grep -w "${_IP}" >/dev/null 2>&1 ${CFGFILE}.bak; then
 				continue
 			fi
 			get_free_aliasid
-			create_config ${ip} ${IFNUM}
+			create_config ${_IP} ${_NETMASK} ${_MASK} ${IFNUM}
 		done
 		mv -f ${CFGFILE}.bak ${CFGFILE}
 	fi

etc/dists/scripts/etcnet-add_ip.sh
@@ -77,9 +77,10 @@ setup_network()
 create_alias()
 {
 	local ip="$1"; shift
+	local mask="$1"; shift
 	local ifnum="$1"; shift
 
-	echo "$ip/32 label $VENET_DEV:$ifnum" >>".tmp/$VENET_DEV/ipv4address" ||
+	echo "$ip/$mask label $VENET_DEV:$ifnum" >>".tmp/$VENET_DEV/ipv4address" ||
 		error "Cannot create .tmp/$VENET_DEV/ipv4address" ${VZ_FS_NO_DISK_SPACE}
 }
 
@@ -121,10 +122,11 @@ add_ip()
 	if [ $# -gt 0 ]; then
 		backup_configs
 
-		local i=0 ip
-		for ip; do
+		local i=0 ipm
+		for ipm; do
+			ip_conv $ipm
 			i="$(find_unused_alias "$(($i+1))")"
-			create_alias "$ip" "$i"
+			create_alias "$_IP" "$_MASK" "$i"
 		done
 
 		move_configs

etc/dists/scripts/etcnet-del_ip.sh
@@ -56,11 +56,12 @@ del_ip()
 		return 0
 	fi
 
-	local ip quoted
-	for ip; do
-		quoted="$(quote_sed_regexp "$ip")"
-		sed -i -e "/^$quoted\/32/d" "$VENET_DEV/ipv4address"
-		ip addr del dev "$VENET_DEV" "$ip/32"
+	local ipm quoted
+	for ipm; do
+		ip_conv $ipm
+		quoted="$(quote_sed_regexp "$_IP/$_MASK")"
+		sed -i -e "/^$quoted/d" "$VENET_DEV/ipv4address"
+		ip addr del dev "$VENET_DEV" "$_IP/$_MASK"
 	done
 
 	if [ $# -gt 0 -a -d /etc/hooks/del_ip.d ] &&

etc/dists/scripts/functions
@@ -362,3 +362,44 @@ function change_hostname()
 	fi
 	mv -f ${cfg}.$$ ${cfg}
 }
+
+function cidr2mask() {
+	local i mask=""
+	local full_octets=$(($1/8))
+	local partial_octet=$(($1%8))
+
+	for i in 0 1 2 3; do
+		if [ $i -lt $full_octets ]; then
+			mask="${mask}255"
+		elif [ $i -eq $full_octets ]; then
+			mask="${mask}$((256 - (1 << (8 - $partial_octet))))"
+		else
+			mask="${mask}0"
+		fi
+		test $i -lt 3 && mask="${mask}."
+	done
+
+	echo $mask
+}
+
+function ip_conv() {
+	local ipm=$1
+	_IP=${ipm%%/*}
+	_MASK=''
+	_NETMASK=''
+	_IPV6ADDR=''
+	if if [ "${_IP#*:}" != "${_IP}" ]; then
+		_IPV6_ADDR=yes
+	fi
+	if echo "${ipm}" | grep -q '/'; then
+		_MASK=${ipm##*/}
+	else
+		if [ -z "$IPV6_ADDR" ]; then
+			_MASK=24
+		else
+			_MASK=128
+		fi
+	fi
+	echo $ipm | grep -q ':' && return
+	_NETMASK=$(cidr2mask $_MASK)
+}

commit 07d74e734fe046c2cf8468bce8682f087dc64d15

etc/dists/scripts/debian-del_ip.sh: fix a typo
Commit 57842cb82 introduced a non-closed quote. Fix it.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

etc/dists/scripts/debian-del_ip.sh
@@ -29,7 +29,7 @@ function del_ip()
 
 	for ipm in ${IP_ADDR}; do
 		ip_conv $ipm
-		if [ -z "$_IPV6ADDR ]; then
+		if [ -z "$_IPV6ADDR" ]; then
 		    ifname=`grep -B 1 -w "${_IP}" ${CFGFILE} |
 				grep "${VENET_DEV}:" | cut -d' ' -f2`
 		    if [ -n "${ifname}" ]; then