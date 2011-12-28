kolyshkin/vzctl

commit 95b0f03ddf61496239afef95ec5b6a942dfd8ba3

vzcfgvalidate: Free strduped parameter
We should free memory allocated by strdup
in any case.

Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

src/vzcfgvalidate.c
@@ -44,7 +44,8 @@ int main(int argc, char **argv)
 	vps_param *param, *gparam;
 	struct stat st;
 	char *infile = NULL;
-	int ret, opt, recover = 0, ask = 0;
+	int opt, recover = 0, ask = 0;
+	int ret = 1;
 
 	while ((opt = getopt(argc, argv, "rih")) > 0) {
 		switch(opt) {
@@ -80,21 +81,24 @@ int main(int argc, char **argv)
 	if (stat(infile, &st)) {
 		logger(-1, 0, "Container configuration file %s not found",
 			infile);
-		free(infile);
-		exit(1);
+		goto err;
 	}
 	param = init_vps_param();
 	if (vps_parse_config(0, infile, param, NULL))
-		exit(1);
+		goto err;
 
 	/* Merge configs (needed for DISK_QUOTA value, maybe others) */
 	merge_vps_param(gparam, param);
 
-	if (!(ret = validate(&param->res, recover, ask)))	{
+	if (!(ret = validate(&param->res, recover, ask))) {
 		if (recover || ask)
 			if (vps_save_config(0, infile, param, NULL, NULL))
-				return 1;
+				goto err;
 		logger(-1, 0, "Validation completed: success");
 	}
+	ret = 0;
+
+err:
+	free(infile);
 	exit(ret);
 }

commit 66c1ee0cae1096a0fc010286038080e853f2e6a8

vzcfgvalidate: fix exit code in case validation failed
vzcfgvalidate(8) manual page says exit code of 2 is expected
in case configuration is invalid.

Commit 95b0f03 accidentally broke that functionality, but even
before it the exit code in this case was 1 not 2.

Fix the behavior to comply with the man page.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzcfgvalidate.c
@@ -102,8 +102,10 @@ int main(int argc, char **argv)
 			if (vps_save_config(0, infile, param, NULL, NULL))
 				goto err;
 		logger(-1, 0, "Validation completed: success");
+		ret = 0;
 	}
-	ret = 0;
+	else
+		ret = 2;
 
 err:
 	free(infile);