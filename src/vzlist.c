kolyshkin/vzctl

commit b199f80f8b4080010c1ea510c5e60982ca729d14

vzlist: fix smart_name alignment
It should be right-aligned, the same as CTID.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzlist.c
@@ -100,7 +100,7 @@ static void print_ ## funcname(struct Cveinfo *p, int index)	\
 	if (p->fieldname != NULL) \
 		str = p->fieldname; \
 	r = snprintf(p_outbuffer, e_buf - p_outbuffer, \
-		"%-" #length "s", str); \
+		"%" #length "s", str); \
 	if (!is_last_field) \
 		r = length; \
 	p_outbuffer += r; \
@@ -109,12 +109,12 @@ static void print_ ## funcname(struct Cveinfo *p, int index)	\
 #define PRINT_STR_FIELD(name, length)				\
 	PRINT_STR_FIELD_FNAME(name, name, length)
 
-PRINT_STR_FIELD(private, 32)
-PRINT_STR_FIELD(root, 32)
-PRINT_STR_FIELD(hostname, 32)
-PRINT_STR_FIELD(name, 32)
-PRINT_STR_FIELD(description, 32)
-PRINT_STR_FIELD(ostemplate, 32)
+PRINT_STR_FIELD(private, -32)
+PRINT_STR_FIELD(root, -32)
+PRINT_STR_FIELD(hostname, -32)
+PRINT_STR_FIELD(name, -32)
+PRINT_STR_FIELD(description, -32)
+PRINT_STR_FIELD(ostemplate, -32)
 PRINT_STR_FIELD_FNAME(name_short, name, 10)
 
 static void print_ip(struct Cveinfo *p, int index)

commit 8dd69cc9989902821c806157ae07d097c5594494

vzlist: fix PRINT_STR_FIELD
It was broken by commit b199f80. A negative value was passed
so p_outbuffer was decremented rather than incremented.

Before this patch, vzlist could not show fields that go after
any of those for which we use PRINT_STR_FIELD.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzlist.c
@@ -124,7 +124,7 @@ static void print_ ## funcname(struct Cveinfo *p, int index)	\
 	r = snprintf(p_outbuffer, e_buf - p_outbuffer,		\
 		"%" #length "s", str);				\
 	if (!is_last_field)					\
-		r = length;					\
+		r = abs(length);				\
 	p_outbuffer += r;					\
 }
 