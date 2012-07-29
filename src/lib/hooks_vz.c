kolyshkin/vzctl

commit a7a9d87d0527825d42bea4e45f13f562d8010254

Move ip handling to VZ-specific file
Call sites are converted.

Signed-off-by: Glauber Costa <glommer@parallels.com>

src/lib/hooks_vz.c
@@ -25,8 +25,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <netinet/in.h>
 #include <sys/personality.h>
 #include <linux/vzcalluser.h>
+#include <linux/vzctl_venet.h>
 
 #include "env.h"
 #include "util.h"
@@ -344,6 +346,65 @@ static int vz_netdev_ctl(vps_handler *h, envid_t veid, int op, char *name)
 	return 0;
 }
 
+static int vz_ip_ctl(vps_handler *h, envid_t veid, int op, const char *ipstr)
+{
+	struct vzctl_ve_ip_map ip_map;
+	int family;
+	unsigned int ip[4];
+	int ret;
+
+	union {
+		struct sockaddr_in  a4;
+		struct sockaddr_in6 a6;
+	} addr;
+
+	if ((family = get_netaddr(ipstr, ip)) < 0)
+		return 0;
+
+
+	if (family == AF_INET) {
+		addr.a4.sin_family = AF_INET;
+		addr.a4.sin_addr.s_addr = ip[0];
+		addr.a4.sin_port = 0;
+		ip_map.addrlen = sizeof(addr.a4);
+	} else if (family == AF_INET6) {
+		addr.a6.sin6_family = AF_INET6;
+		memcpy(&addr.a6.sin6_addr, ip, 16);
+		addr.a6.sin6_port = 0;
+		ip_map.addrlen = sizeof(addr.a6);
+	} else {
+		return -EAFNOSUPPORT;
+	}
+
+	ip_map.veid = veid;
+	ip_map.op = op;
+	ip_map.addr = (struct sockaddr*) &addr;
+
+	ret = ioctl(h->vzfd, VENETCTL_VE_IP_MAP, ip_map);
+	if (ret) {
+		switch (errno) {
+			case EADDRINUSE:
+				ret = VZ_IP_INUSE;
+				break;
+			case ESRCH:
+				ret = VZ_VE_NOT_RUNNING;
+				break;
+			case EADDRNOTAVAIL:
+				if (op == VE_IP_DEL)
+					return 0;
+				ret = VZ_IP_NA;
+				break;
+			default:
+				ret = VZ_CANT_ADDIP;
+				break;
+		}
+		logger(-1, errno, "Unable to %s IP %s",
+			op == VE_IP_ADD ? "add" : "del", ipstr);
+	}
+	return ret;
+
+}
+
 int vz_do_open(vps_handler *h)
 {
 	if ((h->vzfd = open(VZCTLDEV, O_RDWR)) < 0) {
@@ -371,6 +432,7 @@ int vz_do_open(vps_handler *h)
 	h->setcontext = vz_setluid;
 	h->setdevperm = vz_set_devperm;
 	h->netdev_ctl = vz_netdev_ctl;
+	h->ip_ctl = vz_ip_ctl;
 	return 0;
 err:
 	close(h->vzfd);

include/types.h
@@ -103,6 +103,7 @@ typedef struct vps_handler {
 	int (*setcontext)(envid_t veid);
 	int (*setdevperm)(struct vps_handler *h, envid_t veid, struct dev_res *res);
 	int (*netdev_ctl)(struct vps_handler *h, envid_t veid, int op, char *name);
+	int (*ip_ctl)(struct vps_handler *h, envid_t veid, int op, const char *ipstr);
 } vps_handler;
 
 static inline int is_vz_kernel(vps_handler *h)

src/lib/net.c
@@ -83,41 +83,8 @@ int merge_ip_list(int delall, list_head_t *old, list_head_t *add,
 	return __merge_str_list(delall, old, add, del, merged, find_ip);
 }
 
-static inline int _ip_ctl(vps_handler *h, envid_t veid, int op,
-			  unsigned int *ip, int family)
-{
-	struct vzctl_ve_ip_map ip_map;
-	union {
-		struct sockaddr_in  a4;
-		struct sockaddr_in6 a6;
-	} addr;
-
-	if (family == AF_INET) {
-		addr.a4.sin_family = AF_INET;
-		addr.a4.sin_addr.s_addr = ip[0];
-		addr.a4.sin_port = 0;
-		ip_map.addrlen = sizeof(addr.a4);
-	} else if (family == AF_INET6) {
-		addr.a6.sin6_family = AF_INET6;
-		memcpy(&addr.a6.sin6_addr, ip, 16);
-		addr.a6.sin6_port = 0;
-		ip_map.addrlen = sizeof(addr.a6);
-	} else {
-		return -EAFNOSUPPORT;
-	}
-
-	ip_map.veid = veid;
-	ip_map.op = op;
-	ip_map.addr = (struct sockaddr*) &addr;
-
-	return ioctl(h->vzfd, VENETCTL_VE_IP_MAP, &ip_map);
-}
-
 static int ip_ctl(vps_handler *h, envid_t veid, int op, char *str)
 {
-	int ret;
-	int family;
-	unsigned int ipaddr[4];
 	const char *ip, *mask;
 
 	mask = strchr(str, '/');
@@ -128,30 +95,8 @@ static int ip_ctl(vps_handler *h, envid_t veid, int op, char *str)
 	else {
 		ip = str;
 	}
-	if ((family = get_netaddr(ip, ipaddr)) < 0)
-		return 0;
-	ret = _ip_ctl(h, veid, op, ipaddr, family);
-	if (ret) {
-		switch (errno) {
-			case EADDRINUSE:
-				ret = VZ_IP_INUSE;
-				break;
-			case ESRCH:
-				ret = VZ_VE_NOT_RUNNING;
-				break;
-			case EADDRNOTAVAIL:
-				if (op == VE_IP_DEL)
-					return 0;
-				ret = VZ_IP_NA;
-				break;
-			default:
-				ret = VZ_CANT_ADDIP;
-				break;
-		}
-		logger(-1, errno, "Unable to %s IP %s",
-			op == VE_IP_ADD ? "add" : "del", ip);
-	}
-	return ret;
+
+	return h->ip_ctl(h, veid, op, ip);
 }
 
 int run_net_script(envid_t veid, int op, list_head_t *ip_h, int state,

commit 0898ee20152f3875394eb369254bf859b8b5d602

Fix VENETCTL_VE_IP_MAP ioctl argument
Bug introduced by commit a7a9d87.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/hooks_vz.c
@@ -381,7 +381,7 @@ static int vz_ip_ctl(vps_handler *h, envid_t veid, int op, const char *ipstr)
 	ip_map.op = op;
 	ip_map.addr = (struct sockaddr*) &addr;
 
-	ret = ioctl(h->vzfd, VENETCTL_VE_IP_MAP, ip_map);
+	ret = ioctl(h->vzfd, VENETCTL_VE_IP_MAP, &ip_map);
 	if (ret) {
 		switch (errno) {
 			case EADDRINUSE: