kolyshkin/vzctl

commit af95fbdfbde42e0abe3b358bcb32beb35ca437d5

vzctl set: add --mount_opts
Ability to specify additional CT filesystem mount options.
Used only for ploop-based CTs, ignored for simfs.

vzctl --help, vzctl.8, ctid.conf.5 and bash_completion modified
accordingly.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzctl.c
@@ -83,7 +83,8 @@ static void usage(int rc)
 "   [--userpasswd <user>:<passwd>]\n"
 "   [--cpuunits <N>] [--cpulimit <N>] [--cpus <N>] [--cpumask <cpus>]\n"
 "   [--diskspace <soft>[:<hard>]] [--diskinodes <soft>[:<hard>]]\n"
-"   [--quotatime <N>] [--quotaugidlimit <N>] [--capability <name>:on|off ...]\n"
+"   [--quotatime <N>] [--quotaugidlimit <N>] [--mount_opts <opt>[,<opt>...]]"
+"   [--capability <name>:on|off ...]\n"
 "   [--devices b|c:major:minor|all:r|w|rw]\n"
 "   [--devnodes device:r|w|rw|none]\n"
 "   [--netif_add <ifname[,mac,host_ifname,host_mac,bridge]]>]\n"

etc/bash_completion.d/vzctl.sh.in
@@ -84,7 +84,7 @@ _vzctl()
 		--dcachesize --numiptent --physpages --swappages \
 		--cpuunits --cpulimit --cpus --cpumask \
 		--iptables --netdev_add --netdev_del --netif_add --netif_del \
-		--diskquota \
+		--diskquota --mount_opts \
 		--diskspace --diskinodes --quotatime --quotaugidlimit \
 		--capability --devnodes --devices --applyconfig \
 		--name --ioprio --setmode --meminfo --bootorder --features \

include/fs.h
@@ -44,6 +44,7 @@ typedef struct fs_param {
 	char *root;		/**< CT root path. */
 	char *root_orig;	/**< original not expanded root path. */
 	char *tmpl;		/**< TEMPLATE path. */
+	char *mount_opts;	/**< additional mount options (for ploop) */
 	int flags;
 } fs_param;
 

include/vzctl_param.h
@@ -145,6 +145,7 @@
 #define PARAM_SNAPSHOT_SKIP_SUSPEND	373
 
 #define PARAM_SKIP_UMOUNT	374
+#define PARAM_MOUNT_OPTS	396
 
 #define PARAM_LINE		"e:p:f:t:i:l:k:a:b:n:x:h"
 #endif

man/ctid.conf.5.in
@@ -39,6 +39,8 @@ Corresponds to the \fB--ostemplate\fR option.
 Corresponds to the \fB--root\fR option.
 .IP \fBVE_PRIVATE\fR="\fIdirectory\fR"
 Corresponds to the \fB--private\fR option.
+.IP \fBMOUNT_OPTS\fR="\fIoption\fR[,\fIoption\fR...]"
+Corresponds to \fB--mount_opts\fR option.
 .IP \fBDISABLED\fR="\fByes\fR|\fBno\fR"
 Corresponds to the \fB--disabled\fR option.
 .IP \fBORIGIN_SAMPLE\fR="\fIname\fR"

man/vzctl.8.in
@@ -6,7 +6,7 @@
 .ne (2v + 1u)
 .if \\n[.$] \&\\$*
 ..
-.TH vzctl 8 "6 Apr 2012" "OpenVZ" "Containers"
+.TH vzctl 8 "8 Sep 2012" "OpenVZ" "Containers"
 .SH NAME
 vzctl \- perform various operations on an OpenVZ container
 .SH SYNOPSIS
@@ -96,6 +96,7 @@ configuration file \fBvz.conf\fR(5), or to \fB0\fR if not set by
 .OP --bootorder number
 .OP --root path
 .OP --private path
+.OP --mount_opts options
 .OP --userpasswd user\fR:\fIpass
 .OP --disabled \fByes\fR|\fBno
 .OP --name name
@@ -230,6 +231,10 @@ Sets the path to private directory (\fBVE_PRIVATE\fR) for this container.
 This is a directory in which all the container's files are stored.
 Argument can contain literal string \fB$VEID\fR, which will be
 substituted with the numeric CT ID.
+.TP
+\fB--mount_opts\fR \fIoption\fR[\fB,\fIoption\fR...]
+Sets additional mount options for container file system. Only applicable
+for \fBploop\fR layout, ignored otherwise.
 .TP
 \fB--userpasswd\fR \fIuser\fR:\fIpassword\fR
 Sets password for the given user in a container, creating the user if

src/lib/config.c
@@ -112,6 +112,7 @@ static vps_config config[] = {
 {"VE_ROOT",	NULL, PARAM_ROOT},
 {"VE_PRIVATE",	NULL, PARAM_PRIVATE},
 {"TEMPLATE",	NULL, PARAM_TEMPLATE},
+{"MOUNT_OPTS",	NULL, PARAM_MOUNT_OPTS},
 {"VE_LAYOUT",	NULL, PARAM_VE_LAYOUT},
 /*	template     */
 {"OSTEMPLATE",	NULL, PARAM_OSTEMPLATE},
@@ -927,6 +928,9 @@ static int store_fs(vps_param *old_p, vps_param *vps_p, vps_config *conf,
 		break;
 	case PARAM_PRIVATE:
 		ret = conf_store_str(conf_h, conf->name, fs->private_orig);
+		break;
+	case PARAM_MOUNT_OPTS:
+		ret = conf_store_str(conf_h, conf->name, fs->mount_opts);
 		break;
 	}
 	return ret;
@@ -2189,6 +2193,9 @@ static int parse(envid_t veid, vps_param *vps_p, char *val, int id)
 	case PARAM_SNAPSHOT_SKIP_SUSPEND:
 		vps_p->snap.flags |= SNAPSHOT_SKIP_SUSPEND;
 		break;
+	case PARAM_MOUNT_OPTS:
+		ret = conf_parse_str(&vps_p->res.fs.mount_opts, val);
+		break;
 	case PARAM_IGNORED:
 		/* Well known but ignored parameter */
 		break;
@@ -2653,6 +2660,7 @@ static void free_fs(fs_param *fs)
 	FREE_P(fs->private)
 	FREE_P(fs->private_orig)
 	FREE_P(fs->tmpl)
+	FREE_P(fs->mount_opts)
 }
 
 static void free_tmpl(tmpl_param *tmpl)
@@ -2801,6 +2809,7 @@ static void merge_fs(fs_param *dst, fs_param *src)
 	MERGE_STR(private)
 	MERGE_STR(private_orig)
 	MERGE_STR(tmpl)
+	MERGE_STR(mount_opts)
 	MERGE_INT(flags)
 }
 

src/lib/fs.c
@@ -53,6 +53,7 @@ int fsmount(envid_t veid, fs_param *fs, dq_param *dq)
 
 			param.target = fs->root;
 			param.quota = is_2nd_level_quota_on(dq);
+			param.mount_data = fs->mount_opts;
 			ret = vzctl_mount_image(fs->private, &param);
 		}
 #else

commit e383a5e2d6a135938323cea18432a48ceb84273c

vzctl --help: fixed
Broken by commit af95fbd

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzctl.c
@@ -83,7 +83,7 @@ static void usage(int rc)
 "   [--userpasswd <user>:<passwd>]\n"
 "   [--cpuunits <N>] [--cpulimit <N>] [--cpus <N>] [--cpumask <cpus>]\n"
 "   [--diskspace <soft>[:<hard>]] [--diskinodes <soft>[:<hard>]]\n"
-"   [--quotatime <N>] [--quotaugidlimit <N>] [--mount_opts <opt>[,<opt>...]]"
+"   [--quotatime <N>] [--quotaugidlimit <N>] [--mount_opts <opt>[,<opt>...]]\n"
 "   [--capability <name>:on|off ...]\n"
 "   [--devices b|c:major:minor|all:r|w|rw]\n"
 "   [--devnodes device:r|w|rw|none]\n"