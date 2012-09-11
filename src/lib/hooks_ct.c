452329c5b56e429387332ae8edf3e522ed29324f
Kir Kolyshkin <kir@openvz.org>
2012-09-11 05:40:16 +0400

    src/lib/hooks_ct.c: fix compiling with old includes
    When trying to compile on RHEL5 box, this is what we have:
    
      CC     hooks_ct.lo
    hooks_ct.c: In function 'ct_env_create':
    hooks_ct.c:120: error: 'CLONE_NEWUTS' undeclared (first use in this
    function)
    hooks_ct.c:120: error: (Each undeclared identifier is reported only once
    hooks_ct.c:120: error: for each function it appears in.)
    hooks_ct.c:120: error: 'CLONE_NEWPID' undeclared (first use in this
    function)
    hooks_ct.c:120: error: 'CLONE_NEWIPC' undeclared (first use in this
    function)
    hooks_ct.c:121: error: 'CLONE_NEWNET' undeclared (first use in this
    function)
    
    Fix by adding proper defines
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- src/lib/hooks_ct.c
+++ src/lib/hooks_ct.c
@@ -40,6 +40,23 @@ static int sys_setns(int fd, int nstype)
 
 #endif /* ! HAVE_SETNS */
 
+/* These comes from bits/sched.h */
+#ifndef CLONE_NEWUTS
+# define CLONE_NEWUTS   0x04000000      /* New utsname group.  */
+#endif
+#ifndef CLONE_NEWIPC
+# define CLONE_NEWIPC   0x08000000      /* New ipcs.  */
+#endif
+#ifndef CLONE_NEWUSER
+# define CLONE_NEWUSER  0x10000000      /* New user namespace.  */
+#endif
+#ifndef CLONE_NEWPID
+# define CLONE_NEWPID   0x20000000      /* New pid namespace.  */
+#endif
+#ifndef CLONE_NEWNET
+# define CLONE_NEWNET   0x40000000      /* New network namespace.  */
+#endif
+
 static int ct_is_run(vps_handler *h, envid_t veid)
 {
 	return container_is_running(veid);
