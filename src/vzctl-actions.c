kolyshkin/vzctl

commit 287d0dcf92b179f88ebaba454de398d90f2d3f68

vzctl set --mount_opts: warn if changing on running CT
In fact, we could try doing remounting with new options, but it's
a bit problematic since some mount options are set inside ploop lib so
we have to get the old mount options, merge those with the new ones and
then do remount. I am leaving it as a homework for someone who needs
changing mount options runtime.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzctl-actions.c
@@ -744,6 +744,14 @@ static int check_set_mode(vps_handler *h, envid_t veid, int setmode, int apply,
 					"on a running container");
 		found++;
 	}
+	/* Changing mount_opts */
+	if ( new_res->fs.mount_opts && strcmp(new_res->fs.mount_opts,
+				old_res->fs.mount_opts)) {
+		if (loud)
+			logger(-1, 0, "Unable to change mount options "
+					"on a running container");
+		found++;
+	}
 
 	if (!found)
 		return 0;

commit f8fb3745a0bdc476758426de4da5e3435a098d0c

vzctl set --mount_opts: fix a segfault
This (pretty stupid) bug was introduced in commit 287d0dc,
and led to segfault when initially setting mount options
for a running container.

http://bugzilla.openvz.org/2385

Reported-by: Dmitry Kopytov <kopytov@kopytov.ru>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/vzctl-actions.c
@@ -754,8 +754,9 @@ static int check_set_mode(vps_handler *h, envid_t veid, int setmode, int apply,
 		found++;
 	}
 	/* Changing mount_opts */
-	if ( new_res->fs.mount_opts && strcmp(new_res->fs.mount_opts,
-				old_res->fs.mount_opts)) {
+	if ( new_res->fs.mount_opts && (!old_res->fs.mount_opts ||
+			strcmp(new_res->fs.mount_opts,
+				old_res->fs.mount_opts))) {
 		if (loud)
 			logger(-1, 0, "Unable to change mount options "
 					"on a running container");