69e8c7b216b455cd2c2ce991e4b9fe2f98535f68
Kir Kolyshkin <kir@openvz.org>
2013-02-15 13:00:36 -0800

    etc/dists/scripts/postcreate.sh: Fedora 18 support
    For Fedora 18+, we need to set the following caps:
    
    /usr/sbin/arping = cap_net_raw+ep
    /usr/sbin/clockdiff = cap_net_raw+ep
    /usr/bin/systemd-detect-virt = cap_dac_override,cap_sys_ptrace+ep
    
    ALSO NOTE that in Fedora 17+ ping comes with CAP_NET_ADMIN which is
    needed to use ping -m being non-root. But since CAP_NET_ADMIN is not
    granted to a container by default, ping with this capability set
    will not work at all. So we are NOT setting this cap for ping.
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- etc/dists/scripts/postcreate.sh
+++ etc/dists/scripts/postcreate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#  Copyright (C) 2000-2012, Parallels, Inc. All rights reserved.
+#  Copyright (C) 2000-2013, Parallels, Inc. All rights reserved.
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -129,6 +129,11 @@ set_file_caps()
 	_sc 0sAQAAAgkAAAAAAAAAAAAAAAAAAAA= /usr/libexec/pt_chown
 	_sc 0sAQAAAsIAAAAAAAAAAAAAAAAAAAA= /usr/sbin/suexec
 	_sc 0sAQAAAgAgAAAAAAAAAAAAAAAAAAA= /bin/ping /bin/ping6
+
+	# also, for Fedora 18+
+	grep -qEw '1[89]' ${VE_ROOT}/etc/fedora-release 2>/dev/null || return
+	_sc 0sAQAAAgAgAAAAAAAAAAAAAAAAAAA= /usr/sbin/arping /usr/sbin/clockdiff
+	_sc 0sAQAAAgIACAAAAAAAAAAAAAAAAAA= /usr/bin/systemd-detect-virt
 }
 
 [ -z "${VE_ROOT}" ] && exit 1
