kolyshkin/vzctl

commit 784b2caf5a4095716e3deb0962e77fc21377fb3b

ct_restore(): pass SKIP_ACTION_SCRIPT to vps_create_custom
This is the same fix as commit 40ea0d5, but for restoring
upstream containers through criu.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/hooks_ct.c
@@ -963,7 +963,9 @@ err_destroy:
 static int ct_restore(vps_handler *h, envid_t veid, vps_param *vps_p, int cmd,
 	cpt_param *param, skipFlags skip)
 {
-	return vps_start_custom(h, veid, vps_p, SKIP_CONFIGURE | skip, NULL, ct_env_restore, param);
+	return vps_start_custom(h, veid, vps_p,
+			SKIP_CONFIGURE | SKIP_ACTION_SCRIPT | skip,
+			NULL, ct_env_restore, param);
 }
 
 int ct_do_open(vps_handler *h, vps_param *param)

commit 0f5a800cd53a3f816b218e343b93aec2b07c3a12

vzctl restore/start: fix running mount script
When doing vzctl restore, we should still run mount script.
Error was introduced in commits 784b2ca and 40ea0d5. Revert those.

Instead, we check for fn != NULL in vps_start_custom to distinguish
between start and restore. In case of restore, we do not run start script.

https://bugzilla.openvz.org/2603

Reported-by: dippo4589@gmail.com
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/env.c
@@ -660,7 +660,7 @@ int vps_start_custom(vps_handler *h, envid_t veid, vps_param *param,
 	{
 		goto err;
 	}
-	if (!(skip & SKIP_ACTION_SCRIPT)) {
+	if (!(skip & SKIP_ACTION_SCRIPT) && !fn) {
 
 		/* Run dist actions PRE_START script */
 		if (actions.pre_start) {

src/lib/hooks_ct.c
@@ -971,7 +971,7 @@ static int ct_restore(vps_handler *h, envid_t veid, vps_param *vps_p, int cmd,
 	cpt_param *param, skipFlags skip)
 {
 	return vps_start_custom(h, veid, vps_p,
-			SKIP_CONFIGURE | SKIP_ACTION_SCRIPT | skip,
+			SKIP_CONFIGURE | skip,
 			NULL, ct_restore_fn, param);
 }
 

src/lib/hooks_vz.c
@@ -833,7 +833,7 @@ static int vz_restore(vps_handler *h, envid_t veid, vps_param *vps_p,
 	param->rst_fd = rst_fd;
 	param->cmd = cmd;
 	ret = vps_start_custom(h, veid, vps_p,
-			SKIP_CONFIGURE | SKIP_ACTION_SCRIPT | skip,
+			SKIP_CONFIGURE | skip,
 			NULL, restore_fn, param);
 	if (ret)
 		goto err;