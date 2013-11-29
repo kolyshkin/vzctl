kolyshkin/vzctl

commit 32676b4f5d8719744ad235bd10552653d3440b46

destroydir(): really fix lock
Commit b87d455 didn't fix the issue. My bad.

Found by Coverity, CID 996837

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/destroy.c
@@ -225,11 +225,11 @@ static int destroydir(char *dir)
 		exit(0);
 	} else if (pid < 0)  {
 		logger(-1, errno, "destroydir: Unable to fork");
-		_unlock(fd_lock, buf);
 		ret = VZ_RESOURCE_ERROR;
 	}
 	sleep(1);
 	sigaction(SIGCHLD, &actold, NULL);
+	_unlock(fd_lock, buf);
 	return ret;
 }
 

commit 44c25f45af658333ee56d1eef7b01b30b8545ebd

vzctl destroy: fix locking
vzctl destroy is performed asyncronously, i.e. we move the
private area to a temporary location, fork a background
process which then runs rm -rf which can take a lot of time.
Locking is introduced to avoid having multiple background
processes doing removal in parallel.

Commits b87d455 and then 32676b4f broke the locking logic.
Unlocking is supposed to be done on exit, so there's no reason
to do it explicitly.

https://bugzilla.openvz.org/2814

Reported-by: ton <donatas.abraitis@gmail.com>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/destroy.c
@@ -229,7 +229,7 @@ int destroydir(char *dir)
 	}
 	sleep(1);
 	sigaction(SIGCHLD, &actold, NULL);
-	_unlock(fd_lock, buf);
+
 	return ret;
 }
 