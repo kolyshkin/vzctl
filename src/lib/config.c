kolyshkin/vzctl

commit 1b6cb0c2e4f0fc51658a5b62dcafc558a62e88ad

parse_netif_str(): improve NETIF= param parsing
Function parse_netif_str() is used to parse a value of NETIF=
configuration filefile parameter.

Network interface name can be up to IFNAMSIZE long, and while '\0' at
the end is not required, kernel discards the last character and puts
'\0', like this:

	memcpy(dev_name, name, IFNAMSIZ - 1);
	dev_name[IFNAMSIZ - 1] = '\0';

Therefore, we should error out if interface name length is longer than
IFNAMSIZE - 1. While at it, use snprintf to make sure it is
null-terminated (same as it is implemented in parse_netif_str_cmd()).

Reported-by: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/config.c
@@ -1662,17 +1662,17 @@ static int parse_netif_str(envid_t veid, const char *str, veth_dev *dev)
 		if (!strncmp("ifname=", p, 7)) {
 			p += 7;
 			len = next - p;
-			if (len > IFNAMSIZE)
+			if (len + 1 > IFNAMSIZE)
 				return ERR_INVAL;
 			if (dev->dev_name_ve[0] == '\0')
-				strncpy(dev->dev_name_ve, p, len);
+				snprintf(dev->dev_name_ve, IFNAMSIZE, "%s", p);
 		} else if (!strncmp("host_ifname=", p, 12)) {
 			p += 12;
 			len = next - p;
-			if (len > IFNAMSIZE)
+			if (len + 1 > IFNAMSIZE)
 				return ERR_INVAL;
 			if (dev->dev_name[0] == '\0')
-				strncpy(dev->dev_name, p, len);
+				snprintf(dev->dev_name, IFNAMSIZE, "%s", p);
 		} else if (!strncmp("mac=", p, 4)) {
 			p += 4;
 			len = next - p;
@@ -1710,10 +1710,10 @@ static int parse_netif_str(envid_t veid, const char *str, veth_dev *dev)
 		} else if (!strncmp("bridge=", p, 7)) {
 			p += 7;
 			len = next - p;
-			if (len > IFNAMSIZE)
+			if (len + 1 > IFNAMSIZE)
 				return ERR_INVAL;
 			if (dev->dev_bridge[0] == '\0')
-				strncpy(dev->dev_bridge, p, len);
+				snprintf(dev->dev_bridge, IFNAMSIZE, "%s", p);
 		}
 		p = ++next;
 	} while (p < ep);

commit ef23e3153ec5c8c227abf8fec64e0a7697062e4e

Fix parse_netif_str()
Broken by commit 1b6cb0c

https://bugzilla.openvz.org/2867

Reported-by: Andrey Vagin <avagin@openvz.org>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/config.c
@@ -1665,14 +1665,14 @@ static int parse_netif_str(envid_t veid, const char *str, veth_dev *dev)
 			if (len + 1 > IFNAMSIZE)
 				return ERR_INVAL;
 			if (dev->dev_name_ve[0] == '\0')
-				snprintf(dev->dev_name_ve, IFNAMSIZE, "%s", p);
+				snprintf(dev->dev_name_ve, len + 1, "%s", p);
 		} else if (!strncmp("host_ifname=", p, 12)) {
 			p += 12;
 			len = next - p;
 			if (len + 1 > IFNAMSIZE)
 				return ERR_INVAL;
 			if (dev->dev_name[0] == '\0')
-				snprintf(dev->dev_name, IFNAMSIZE, "%s", p);
+				snprintf(dev->dev_name, len + 1, "%s", p);
 		} else if (!strncmp("mac=", p, 4)) {
 			p += 4;
 			len = next - p;
@@ -1713,7 +1713,7 @@ static int parse_netif_str(envid_t veid, const char *str, veth_dev *dev)
 			if (len + 1 > IFNAMSIZE)
 				return ERR_INVAL;
 			if (dev->dev_bridge[0] == '\0')
-				snprintf(dev->dev_bridge, IFNAMSIZE, "%s", p);
+				snprintf(dev->dev_bridge, len + 1, "%s", p);
 		}
 		p = ++next;
 	} while (p < ep);