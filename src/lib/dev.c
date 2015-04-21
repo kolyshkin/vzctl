kolyshkin/vzctl

commit c2a48a7e24e1956fef1251001e59121782002e1e

add_dev_param(): don't modify second argument
Commit ea64ee6 has a bad side effect: after calling merge_dev()
the device names in vps_p list (which is supposed to have configuration
as read from ve.conf) disappear. This is caused by the hunk modifying
add_dev_param().

This patch fixes that side effect, also marking the second argument
const so this won't happen in the future.

This is not a bug fix per se (as no one was using dev in vps_p list),
but is a requirement for the next commit.

Based on a patch by Andrey Vagin <avagin@openvz.org>:
  https://bugzilla.openvz.org/3186#c3

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/dev.c
@@ -177,7 +177,7 @@ out:
 	return ret;
 }
 
-int add_dev_param(dev_param *dev, dev_res *res)
+int add_dev_param(dev_param *dev, const dev_res *res)
 {
 	dev_res *tmp;
 
@@ -187,8 +187,12 @@ int add_dev_param(dev_param *dev, dev_res *res)
 	if (list_is_init(&dev->dev))
 		list_head_init(&dev->dev);
 	memcpy(tmp, res, sizeof(*tmp));
+	tmp->name = strdup(res->name);
+	if (tmp->name == NULL) {
+		free(tmp);
+		return -1;
+	}
 	list_add_tail(&tmp->list, &dev->dev);
-	res->name = NULL;
 
 	return 0;
 }

include/dev.h
@@ -59,7 +59,7 @@ int create_static_dev(const char *root, const char *name, const char *alias,
 int vps_set_devperm(vps_handler *h, envid_t veid, const char *root,
 		dev_param *dev);
 int set_devperm(vps_handler *h, envid_t veid, dev_res *dev);
-int add_dev_param(dev_param *dev, dev_res *res);
+int add_dev_param(dev_param *dev, const dev_res *res);
 void free_dev_param(dev_param *dev);
 
 int vps_set_pci(vps_handler *h, envid_t veid, int op, const char *root,

src/lib/config.c
@@ -1330,12 +1330,13 @@ static int parse_devnodes(vps_param *vps_p, char *val)
 	dev_res dev;
 
 	for_each_strtok(token, val, " ") {
+		int ret;
 		if (parse_devnodes_str(token, &dev))
 			return ERR_INVAL;
-		if (add_dev_param(&vps_p->res.dev, &dev)) {
-			free(dev.name);
+		ret = add_dev_param(&vps_p->res.dev, &dev);
+		free(dev.name);
+		if (ret)
 			return ERR_NOMEM;
-		}
 	}
 
 	return 0;

commit fc1f135703961e209f56e56a738284aeabd20839

vzctl set --devices: fix SIGSEGV
This is caused by the commit c2a48a7e. Note add_dev_param() is called
from parse_devices_str() in which name is not set, so we shouldn't
blindly use strdup().

https://bugzilla.openvz.org/3189

Reported-by: Devon <devon.b@virtualcomplete.com>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

src/lib/dev.c
@@ -197,10 +197,12 @@ int add_dev_param(dev_param *dev, const dev_res *res)
 	if (list_is_init(&dev->dev))
 		list_head_init(&dev->dev);
 	memcpy(tmp, res, sizeof(*tmp));
-	tmp->name = strdup(res->name);
-	if (tmp->name == NULL) {
-		free(tmp);
-		return -1;
+	if (res->name) {
+		tmp->name = strdup(res->name);
+		if (tmp->name == NULL) { /* ENOMEM */
+			free(tmp);
+			return -1;
+		}
 	}
 	list_add_tail(&tmp->list, &dev->dev);
 